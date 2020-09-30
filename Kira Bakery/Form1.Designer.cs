6fd3a97abaa75d95c9033eeae722e32e8e75c7e4
Jurugo Felix <jurugofelix123@live.com>
2020-09-30 16:56:19 +0300

    Minor Edits

--- Kira Bakery/Form1.Designer.cs
+++ Kira Bakery/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Kira Bakery";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
